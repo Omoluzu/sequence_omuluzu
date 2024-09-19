dba8756a60f1f44a2bed0d061d0110093092ecd1
volkov.al <volkov.al@tecomgroup.ru>
2024-09-19 07:53:57 +0300

    run sequence async and show process to widget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-from pathlib import Path
 import threading
+from pathlib import Path
 from PySide6.QtWidgets import (
     QApplication, QMainWindow,
     QPushButton, QVBoxLayout,
@@ -9,6 +9,23 @@ from PySide6.QtWidgets import (
 )
 
 from src.sequence.script import sequence, get_sequence_all_name
+from src.sequence import Config
+
+
+class CheckSequenceWidget(QCheckBox):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def input(self, value: str):
+        key, value = value.split('=')
+
+        match key:
+            case 'progress':
+                print(value)
+
+                text = self.text().split(' [')
+                print(text)
+                self.setText(text[0] + ' [' + value + ']')
 
 
 class CheckBoxWidget(QWidget):
@@ -28,18 +45,21 @@ class CheckBoxWidget(QWidget):
         """)
 
     def finds(self):
-        checkboxes: list[QCheckBox] = self.findChildren(QCheckBox)
+        checkboxes: list[CheckSequenceWidget] = self.findChildren(CheckSequenceWidget)
         for checkbox in checkboxes:
             if checkbox.isChecked():
-                self.main.run_sequence(checkbox.text())
+                self.main.run_sequence(checkbox)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Omoluzu Sequence")
+        Config()
 
-        self.select_folder = QPushButton("Выбрать папку")
+        self.setWindowTitle("Omoluzu Sequence")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.select_folder = QPushButton("Выбрать папку")  # todo: Асинхранный выбор папки, а точнее анализа файлов в нем, так как он сейчас блокирует приложение
         self.select_folder.clicked.connect(self.open_folder_dialog)
 
         self.checkbox_widget = CheckBoxWidget(self)
@@ -62,13 +82,15 @@ class MainWindow(QMainWindow):
         if folder_path:
             self.sequences = get_sequence_all_name(Path(folder_path))
             for name in self.sequences.names:
-                checkbox = QCheckBox(name)
+                checkbox = CheckSequenceWidget(name)
                 self.checkbox_widget.layout.addWidget(checkbox)
 
     def run_sequence(self, sequence_name):
         thread = threading.Thread(
             target=sequence,
-            args=(self.sequences.get_sequence(sequence_name),)
+            args=(self.sequences.get_sequence(
+                sequence_name.text()), sequence_name  # todo: В имя текста есть дополнительная информация [end] которая при повторном запуске не находит sequence
+            )
         )
         thread.start()
 
--- src/sequence/script/sequence.py
+++ src/sequence/script/sequence.py
@@ -1,18 +1,58 @@
 import os
+import sys
 import ffmpeg
+
 from src.sequence import SequenceInfo
 
 
-def sequence(sequence_info: SequenceInfo, frame_rate: int = 25) -> None:
+def sequence(
+        sequence_info: SequenceInfo, output_adapter, frame_rate: int = 25
+) -> None:
     """file sequence
     :param sequence_info: Input file
+    :param output_adapter: todo
     :param frame_rate: frame rate
     """
-    ffmpeg.output(
-        ffmpeg.input(
-            os.path.join(sequence_info.full_path, sequence_info.regular),
-            start_number=sequence_info.start_number,
-            framerate=frame_rate
-        ),
-        os.path.join(sequence_info.output_name)
-    ).run(overwrite_output=True)
+
+    process = (
+        ffmpeg.output(
+            ffmpeg.input(
+                os.path.join(sequence_info.full_path, sequence_info.regular),
+                start_number=sequence_info.start_number,
+                framerate=frame_rate
+            ),
+            os.path.join(sequence_info.output_name)
+        ).global_args(
+            '-progress', 'pipe:1'
+        ).run_async(
+            pipe_stdout=True,
+            pipe_stderr=True
+        )
+    )
+
+    while True:
+        output = process.stdout.readline()
+        if output == b'' and process.poll() is not None:
+            break
+        if output:
+            output_adapter.input(output.decode().strip())
+
+            """
+            ---frame=118
+            ---fps=22.59
+            ---stream_0_0_q=-1.0
+            ---bitrate=2841.9kbits/s
+            ---total_size=1634131
+            ---out_time_us=4600078
+            ---out_time_ms=4600078
+            ---out_time=00:00:04.600078
+            ---dup_frames=0
+            ---drop_frames=0
+            ---speed=0.881x
+            ---progress=end
+            """
+
+    # Получение ошибок, если они есть
+    stderr = process.stderr.read()
+    if stderr:
+        print(stderr.decode(), file=sys.stderr)
